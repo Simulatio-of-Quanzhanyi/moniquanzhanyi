6258321e723a7918725711b82d6191ddff41dbc2
DevilMayCry <772953343@qq.com>
2015-07-31 11:42:06 +0800

    it can move now

--- 2015719/Wpf5320/Window_collect1_PointSurvey1.xaml.cs
+++ 2015719/Wpf5320/Window_collect1_PointSurvey1.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using System.Collections;
 using System.Data;
 using System.Data.OleDb;
 
@@ -21,11 +21,17 @@ namespace Wpf5320
     /// </summary>
     public partial class collect1_PointSurvey1 : Window
     {
+        bool isMove = false;
         Point a;
         protected Thickness UCMargin;
         Point StationPoint = new Point(100.968, 100.324);
         Point CurrentPoint = new Point();
+        int pointCount = 0;
+        List<Line> CurrentLine1 = new List<Line>();
+        List<Line> CurrentLine2 = new List<Line>();
+        List<String> CurrentPoints = new List<String>();
         PointData PD = new PointData();
+
         public collect1_PointSurvey1()
         {
             double Hhudu, Vhudu, Dis;
@@ -46,7 +52,8 @@ namespace Wpf5320
 
         private void Window_MouseDown_1(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            Point a = Mouse.GetPosition(this);
+            if (e.LeftButton == MouseButtonState.Pressed && (a.X < 65 || a.X > 380 || a.Y < 76 || a.Y > 318))
             {
                 DragMove();
             }
@@ -108,6 +115,8 @@ namespace Wpf5320
         private void BtSave_Click(object sender, RoutedEventArgs e)
         {
             //规划坐标宽153，长313，以测站为中心
+            pointCount++;
+            CurrentPoints.Add("pt" + pointCount);
             Line stationLine = new Line();
             Line stationLine1 = new Line();
             Line stationLine2 = new Line();
@@ -143,9 +152,12 @@ namespace Wpf5320
             stationLine1.Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 255));
             stationLine2.Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 255));
             stationLine.Stroke = new SolidColorBrush(Color.FromRgb(0, 255, 0));
-            CanvasDraw.Children.Add(stationLine);
-            CanvasDraw.Children.Add(line1);
-            CanvasDraw.Children.Add(line2);
+            CurrentLine1.Add(line1);
+            CurrentLine2.Add(line2);
+
+            CanvasDraw.Children.Add(stationLine);     
+            CanvasDraw.Children.Add(CurrentLine1[pointCount - 1]);
+            CanvasDraw.Children.Add(CurrentLine2[pointCount - 1]);
             CanvasDraw.Children.Add(stationLine2);
             CanvasDraw.Children.Add(stationLine1);
 
@@ -173,20 +185,10 @@ namespace Wpf5320
             ((ScaleTransform)(((TransformGroup)(((UIElement)(this.CanvasDraw)).RenderTransform)).Children[0])).ScaleY = dbl_ZoomY * 0.8;
         }
 
-        private void CanvasDraw_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("ok");
-            this.MouseMove += new MouseEventHandler(CanvasDraw_MouseMove);
-            a = e.GetPosition(this);
-            if (a.X > 0 && a.Y > 0)
-            {
-                UCMargin = CanvasDraw.Margin;
-            }
-        }
 
         private void CanvasDraw_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton != MouseButtonState.Released)
+            if (e.LeftButton == MouseButtonState.Pressed && isMove == true)
             {
                 this.Cursor = Cursors.SizeAll;
                 this.MouseLeftButtonUp += new MouseButtonEventHandler(CanvasDraw_MouseLeftButtonUp);
@@ -202,6 +204,17 @@ namespace Wpf5320
                 this.Cursor = null;
             }
         }
+        private void CanvasDraw_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.MouseMove += new MouseEventHandler(CanvasDraw_MouseMove);
+            a = e.GetPosition(this);
+            
+            if (a.X > 0 && a.Y > 0)
+            {
+                UCMargin = CanvasDraw.Margin;
+            }
+           
+        }
 
         private void CanvasDraw_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
@@ -213,11 +226,23 @@ namespace Wpf5320
         {
             ((ScaleTransform)(((TransformGroup)(((UIElement)(this.CanvasDraw)).RenderTransform)).Children[0])).ScaleX=1.0;
             ((ScaleTransform)(((TransformGroup)(((UIElement)(this.CanvasDraw)).RenderTransform)).Children[0])).ScaleY=1.0;
+            CanvasDraw.Margin = new Thickness(0, 0, 0, 0);
         }
 
         private void BT_Move_Click(object sender, RoutedEventArgs e)
         {
-
+            
+           if(isMove == false)
+           {
+               isMove = true;
+               BT_Move.Background = Brushes.LightBlue;
+ 
+           }
+           else
+           {
+               isMove = false;
+               BT_Move.Background = BT_Fangda.Background;
+           }
         }
 
         private void Bt_Power_Click(object sender, RoutedEventArgs e)
@@ -226,5 +251,6 @@ namespace Wpf5320
             Shutdown_PowerOff.Show();
             this.Close();//关闭当前窗口 
         }
+
     }
 }
